c3576dda74e298c8c79127b18c3f881827fbe44a
Евгений <gg1786@rambler.ru>
2024-09-15 17:06:43 +0300

    Изменеие AccountController, добавление исключений

--- src/main/java/com/example/accountservice/controller/AccountController.java
+++ src/main/java/com/example/accountservice/controller/AccountController.java
@@ -1,38 +1,36 @@
 package com.example.accountservice.controller;
+
 import com.example.accountservice.model.Account;
 import com.example.accountservice.service.AccountService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.security.auth.login.AccountNotFoundException;
 import java.math.BigDecimal;
-import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/accounts")
 public class AccountController {
 
-    @Autowired
-    private AccountService accountService;
+    private final AccountService accountService;
 
-    @PostMapping
-    public ResponseEntity<Account> createAccount(@RequestParam Long userId, @RequestParam String currency) {
-        Account account = accountService.createAccount(userId, currency);
+    @Autowired
+    public AccountController(AccountService accountService) {
+        this.accountService = accountService;
+    }
+
+    @PostMapping("/{accountId}/deposit")
+    public ResponseEntity<Account> deposit(@PathVariable Long accountId,
+                                           @RequestParam BigDecimal amount) throws AccountNotFoundException {
+        Account account = accountService.deposit(accountId, amount);
         return ResponseEntity.ok(account);
     }
 
-    @PostMapping("/{id}/deposit")
-    public ResponseEntity<Account> deposit(@PathVariable Long id,
-                                           @RequestParam BigDecimal amount,
-                                           @RequestParam String currency) {
-        Account updatedAccount = accountService.deposit(id, amount, currency);
-        return ResponseEntity.ok(updatedAccount);
-    }
-
-    @GetMapping("/{id}")
-    public ResponseEntity<Account> getAccountById(@PathVariable Long id) {
-        Optional<Account> account = accountService.getAccountById(id);
-        return account.map(ResponseEntity::ok)
-                .orElseGet(() -> ResponseEntity.notFound().build());
+    @GetMapping("/{accountId}")
+    public ResponseEntity<Account> getAccountById(@PathVariable Long accountId) throws AccountNotFoundException {
+        return accountService.getAccountById(accountId)
+                .map(ResponseEntity::ok)
+                .orElseThrow(() -> new AccountNotFoundException("Аккаунт с id " + accountId + " не найден"));
     }
 }
