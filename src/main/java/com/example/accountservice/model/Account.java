2f3b19779c74465bce4dc034b2351caafde3bdf3
Евгений <gg1786@rambler.ru>
2024-09-15 15:56:03 +0300

    Изменение класса Account, замена коллекции Set  на List, изменены getters и setters

--- src/main/java/com/example/accountservice/model/Account.java
+++ src/main/java/com/example/accountservice/model/Account.java
@@ -1,7 +1,11 @@
 package com.example.accountservice.model;
+
 import jakarta.persistence.*;
+
 import java.math.BigDecimal;
-import java.util.Set;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
 
 @Entity
 public class Account {
@@ -19,8 +23,7 @@ public class Account {
     private String currency;
 
     @OneToMany(mappedBy = "account", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
-    private Set<Transaction> transactions;
-
+    private List<Transaction> transactions = new ArrayList<>();
 
     public Long getId() {
         return id;
@@ -54,12 +57,28 @@ public class Account {
         this.currency = currency;
     }
 
-    public Set<Transaction> getTransactions() {
+    public List<Transaction> getTransactions() {
         return transactions;
     }
 
-    public void setTransactions(Set<Transaction> transactions) {
+    public void setTransactions(List<Transaction> transactions) {
         this.transactions = transactions;
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Account account = (Account) o;
+        return Objects.equals(id, account.id) &&
+                Objects.equals(user, account.user) &&
+                Objects.equals(balance, account.balance) &&
+                Objects.equals(currency, account.currency) &&
+                Objects.equals(transactions, account.transactions);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, user, balance, currency, transactions);
+    }
 }
--- src/test/java/config/PostgresTestConfig.java
+++ src/test/java/config/PostgresTestConfig.java
@@ -1,4 +1,4 @@
-package com.example.accountservice;
+package config;
 
 
 import org.springframework.boot.test.context.TestConfiguration;
