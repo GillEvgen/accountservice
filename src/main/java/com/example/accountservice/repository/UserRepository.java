a2e9d78a40ed67c6354cc0ec64c25232aec230b0
Евгений <gg1786@rambler.ru>
2024-09-15 16:16:47 +0300

    Добавление аннтации @Repository

--- src/main/java/com/example/accountservice/repository/UserRepository.java
+++ src/main/java/com/example/accountservice/repository/UserRepository.java
@@ -2,9 +2,11 @@ package com.example.accountservice.repository;
 
 import com.example.accountservice.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByDocumentNumber(String documentNumber);
 }
